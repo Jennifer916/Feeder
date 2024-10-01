b746ed278e33b0f5a6302ae20e8438554eb8ea3a
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2024-10-01 16:03:12 +0000

    final change

--- Main.java
+++ Main.java
@@ -11,16 +11,16 @@ public class Main
         a.simulateOneDay(22);
         System.out.println(a.getcurrentFood());
          Feeder b=new Feeder(100);
-         b.simulateOneDay(5);
+        b.simulateOneDay(5);
          System.out.println(b);
          Feeder d=new Feeder (2400);
-         d.simulateManyDays(10,4);
+         System.out.println(d.simulateManyDays(10,4));
          System.out.println(d);
          Feeder e= new Feeder(250);
-         e.simulateManyDays(10,5);
+         System.out.println(e.simulateManyDays(10,5));
          System.out.println(e);
          Feeder w= new Feeder (0);
-         w.simulateManyDays(5, 10);
+         System.out.println(w.simulateManyDays(5, 10));
          System.out.println(w);
     }
         
